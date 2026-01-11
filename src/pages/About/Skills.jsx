2bb907c687724d45bfe1cc2afc46861dd372ceba
unknown <funmilolaadebiyi52@gmail.com>
2026-01-11 15:26:25 +0100

    new-ui-updates

--- src/pages/About/Skills.jsx
+++ src/pages/About/Skills.jsx
@@ -1,9 +1,9 @@
 import data from '../../data.json'
 export default function Skills() {
     return (
-        <div className='px-8 md:px-24 pt-10 md:pt-16 font-sans'>
+        <div className='px-8 md:px-24 pt-10 md:pt-16 font-sans pb-10'>
             <h2 className="text-white text-lg md:text-xl text-center font-semibold">Skills</h2>
-            <ul className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-6 items-center justify-center gap-6 md:gap-22 pt-12 md:border-b md:border-b-[#172631] pb-10 md:pb-4 '>
+            <ul className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-6 items-center justify-center gap-6 md:gap-22 pt-12 md:border-b md:border-b-[#172631]  md:pb-4 '>
                 {data.skills.map((skill, index) => (
                     <a href={skill.link} target='_blank'>
                     <li key={index} className='flex flex-col items-center gap-2 px-6 py-2.5 hover:bg-[#172631] hover:rounded-sm'>
